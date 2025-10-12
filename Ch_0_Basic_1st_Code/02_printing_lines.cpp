29fd76674819a224245bd5275bc270140e6d6baf
Muhammad Ayan <mohd.ayanasim.786@gmail.com>
2025-10-12 15:48:17 +0200

    Write some basic for C++ in Chapter 0.

---
+++ Ch_0_Basic_1st_Code/02_printing_lines.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std; 
+
+int main() {
+    cout << "Hello, World!" << endl;
+    cout << "Mohammad Ayan Asim" << "\n";
+    cout << "Learning C++\n";
+    cout << "Just basics\nPowerfull Language\n";
+    cout << "Learning from " << "ApnaCollege" << endl;
+
+    return 0;
+}
