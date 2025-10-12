4e9d9fc4c49695064bc238738fac9bb26a8ec099
Muhammad Ayan <mohd.ayanasim.786@gmail.com>
2025-10-12 18:07:21 +0200

    Learn very basics of C++ in Chapter 0.

---
+++ Ch_0_Basics_of_Cpp/04_comments_in_cpp.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    // This is a single line comment :
+    // cout << "Mohammad" << endl;
+
+    /*
+    This is a multi line comment :
+    
+    Multi line comments use back-slash and star symbol
+
+    cout << "Ayan" << "\n";
+
+    */
+
+    return 0;
+}
---
+++ Ch_0_Basics_of_Cpp/05_input_in_cpp.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int age;
+    cout << "Enter age: ";
+    cin >> age;
+
+    cout << "Your age is: ";
+    cout << age << endl;
+
+    return 0;
+}
