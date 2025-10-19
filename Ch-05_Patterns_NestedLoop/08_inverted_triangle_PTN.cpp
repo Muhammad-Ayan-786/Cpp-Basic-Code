b1989ad275c4b7deec6dee6dab09b3333bc3ef05
Muhammad Ayan <mohd.ayanasim.786@gmail.com>
2025-10-19 16:35:19 +0200

    Print Inverted triangle pattern.

---
+++ Ch-05_Patterns_NestedLoop/08_inverted_triangle_PTN.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 4;
+
+    // Numbers ...
+    for (int i = 0; i < n; i++)
+    {
+        // Spaces
+        for (int j = 0; j < i; j++)
+            cout << " ";
+
+        // Numbers
+        for (int j = 0; j < n - i; j++)
+            cout << (i + 1);
+
+        cout << endl;
+    }
+
+
+    // Characters ...
+    char ch = 'A';
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++) {
+            cout << " ";
+        }
+
+        for (int j = 0; j < n-i; j++) {
+            cout << ch;
+        }
+
+        ch++;
+        cout << endl;
+    }
+
+    return 0;
+}
