b8183407ab687529d53556fed8c54e37ef0ed571
Muhammad Ayan <mohd.ayanasim.786@gmail.com>
2025-10-19 10:47:06 +0200

    Update the Logic for Prime number.

--- Ch-04_Loop_in_Cpp/07_primeNum.cpp
+++ Ch-04_Loop_in_Cpp/07_primeNum.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 int main() {
     int n = 6;
-    int isPrime = 1;
+    bool isPrime = 1;
 
-    for (int i = 2; i <= n-1; i++)
+    for (int i = 2; i * i <= n; i++)
     {
         if (n % i == 0) {
             isPrime = 0;
