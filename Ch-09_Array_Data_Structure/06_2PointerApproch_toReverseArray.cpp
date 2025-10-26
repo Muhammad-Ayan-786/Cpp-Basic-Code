d83ca68aae69b5cdb6f7dbb60c47d3fc95e27b18
Muhammad Ayan <mohd.ayanasim.786@gmail.com>
2025-10-26 17:59:32 +0200

    Write a code in a new file to reverse an Array, using 2 Pointer Approch.

---
+++ Ch-09_Array_Data_Structure/06_2PointerApproch_toReverseArray.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+// Reversing an Array with 2 Pointer Approch the build-in swap().
+void reverseArray (int arr[], int size) {
+    int start = 0;
+    int end = size - 1;
+
+    while (start < end)
+    {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    reverseArray(arr, size);
+    printArray(arr, size);
+
+    return 0;
+}
