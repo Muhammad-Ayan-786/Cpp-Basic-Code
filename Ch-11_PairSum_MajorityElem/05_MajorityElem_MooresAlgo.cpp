e351a20bdc0c0132267176dc7ad2b7cb38499485
Muhammad Ayan <mohd.ayanasim.786@gmail.com>
2025-11-03 11:14:58 +0200

    Write the most optimized code for MJ Elem using Moore's Algo.

---
+++ Ch-11_PairSum_MajorityElem/05_MajorityElem_MooresAlgo.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int MJElem_MooresAlgo(int arr[], int n) {
+    int freq = 0;
+    int ans = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (freq == 0)
+            ans = arr[i];
+
+        if (ans == arr[i])
+            freq++;
+        else
+            freq--;
+    }
+
+    return ans;    
+}
+
+int main() {
+    int arr[] = {1, 2, 2, 1, 1, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int MJ = MJElem_MooresAlgo(arr, n);
+
+    cout << "Majority Elem: " << MJ << endl;
+
+    return 0;
+}
